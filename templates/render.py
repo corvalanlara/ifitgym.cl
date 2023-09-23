b29413522ea73a4c73ed06e04c9d51ffd46211f3
corvalanlara <decorval@uc.cl>
2023-09-23 16:10:56 -0300

    Con 404 page

--- templates/render.py
+++ templates/render.py
@@ -1,7 +1,7 @@
 from jinja2 import Environment, FileSystemLoader
 
 env = Environment(loader=FileSystemLoader('templates'))
-templates = ['index.html', 'planes.html', 'programas.html', 'espacio.html', 'terminos.html']
+templates = ['index.html', 'planes.html', 'programas.html', 'espacio.html', 'terminos.html', '404.html']
 
 for template in templates:
     tmp = env.get_template(template)
