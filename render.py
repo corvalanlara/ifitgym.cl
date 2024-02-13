e1a5d3adc9c146238c8cb171dbf71e49c58cd30d
Daniel Corvalán <daniel@corvalan.dev>
2024-02-13 15:38:31 -0300

    Backing up

--- render.py
+++ render.py
@@ -2,19 +2,13 @@ from jinja2 import Environment, FileSystemLoader
 import os
 import shutil
 
-assets = ['images', 'js', 'css']
-output = '_site'
-os.mkdir(output)
-for folder in assets:
-    shutil.copytree(folder, os.path.join(output, folder))
-
 env = Environment(loader=FileSystemLoader('templates'))
 templates = ['index.html', 'planes.html', 'programas.html', 'espacio.html', 'terminos.html', '404.html', 'nosotros.html']
 
 for template in templates:
     tmp = env.get_template(template)
     out = tmp.render()
-    location = "./_site/{}".format(template)
+    location = "{}".format(template)
     with open(location, "w", encoding="utf-8") as f:
         f.write(out)
 print("Listo")
